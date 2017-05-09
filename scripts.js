d84a353a8d95dac7fcb6f5838aba1ef17255c509
Micah Nisely <mjn@micahs-mbp.attlocal.net>
2017-05-09 07:01:31 -0500

    no message

--- scripts.js
+++ scripts.js
@@ -1,39 +1,59 @@
-document.getElementById("StopWatch").onclick = function() {
+document.getElementById("StartButton").onclick = function() {
   startRace();
+}
+
+document.getElementById("BarryWins").onclick = function() {
+  document.getElementById("BarryWins").style.display = 'none';
+  document.getElementById("StopWatch").style.display = 'none';
+  document.getElementById("StartButton").style.display = 'block';
+  document.getElementById("field").style.display = 'block';
+  document.getElementById("BoJackson").style.left = '30px';
+  document.getElementById("BarrySanders").style.left = '30px';
+}
+
+document.getElementById("BoWins").onclick = function() {
+  document.getElementById("BoWins").style.display = 'none';
+  document.getElementById("StopWatch").style.display = 'none';
+  document.getElementById("StartButton").style.display = 'block';
+  document.getElementById("field").style.display = 'block';
+  document.getElementById("BoJackson").style.left = '30px';
+  document.getElementById("BarrySanders").style.left = '30px';
 }
 
 function startRace() {
   var barry = document.getElementById("BarrySanders");
   var bo = document.getElementById("BoJackson");
+  var startButton = document.getElementById("StartButton");
   var stopWatch = document.getElementById("StopWatch");
   var field = document.getElementById("field");
   var barry_wins = document.getElementById("BarryWins");
   var bo_wins = document.getElementById("BoWins");
+  var finish = document.getElementById("footballfield").clientWidth - 85;
 
-  var posBarry = 0;
-  var posBo = 0;
+  var posBarry = 30;
+  var posBo = 30;
 
   var hasWinner = false;
-  barry.style.display = 'none';
-  bo.style.display = 'none';
+  startButton.style.display = 'none';
+  stopWatch.style.display = 'block';
 
-  var id = setInterval(frame, 500);
+  var id = setInterval(frame, 60);
   function frame()
   {
-    if(posBarry >= 350 && posBo >= 350)
+    if(posBarry >= finish && posBo >= finish)
     {
       clearInterval(id);
       return;
     }
 
-    if(posBarry < 350)
+    if(posBarry < finish)
     {
       posBarry += Math.round(Math.random()*10);
       barry.style.left = posBarry + 'px';
 
-      if(posBarry >= 350)
+      if(posBarry >= finish)
       {
-        posBarry = 350;
+        posBarry = finish;
         if(!hasWinner){
           hasWinner = true;
           field.style.display = 'none';
@@ -43,14 +63,14 @@ function startRace() {
 
     }
 
-    if(posBo < 350)
+    if(posBo < finish)
     {
-      posBo += Math.round(Math.round()*10);
+      posBo += Math.round(Math.random()*10);
       bo.style.left = posBo + 'px';
 
-      if(posBo >= 350)
+      if(posBo >= finish)
       {
-        posBo = 350;
+        posBo = finish;
         if(!hasWinner){
           hasWinner = true;
           field.style.display = 'none';
