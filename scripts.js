87d6d460edeead4e827b2398227cae3b2f6391cf
Micah Nisely <mjn@micahs-mbp.attlocal.net>
2017-05-08 19:42:27 -0500

    no message

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
+document.getElementById("StopWatch").onclick = function() {
+  startRace();
+}
+
 function startRace() {
   var barry = document.getElementById("BarrySanders");
   var bo = document.getElementById("BoJackson");
   var stopWatch = document.getElementById("StopWatch");
+  var field = document.getElementById("field");
+  var barry_wins = document.getElementById("BarryWins");
+  var bo_wins = document.getElementById("BoWins");
 
   var posBarry = 0;
   var posBo = 0;
 
   var hasWinner = false;
-  elementBarryWin.style.display = 'none';
-  elementBoWin.stylse.display = 'none';
+  barry.style.display = 'none';
+  bo.style.display = 'none';
 
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, 500);
   function frame()
   {
     if(posBarry >= 350 && posBo >= 350)
@@ -18,32 +25,39 @@ function startRace() {
       clearInterval(id);
       return;
     }
+
     if(posBarry < 350)
     {
       posBarry += Math.round(Math.random()*10);
+      barry.style.left = posBarry + 'px';
+
       if(posBarry >= 350)
       {
         posBarry = 350;
         if(!hasWinner){
           hasWinner = true;
-          elementBarryWin.style.display = 'unset';
+          field.style.display = 'none';
+          barry_wins.style.display = 'block';
         }
       }
-      barry.style.left = posBarry + 'px';
+
     }
 
     if(posBo < 350)
     {
       posBo += Math.round(Math.round()*10);
+      bo.style.left = posBo + 'px';
+
       if(posBo >= 350)
       {
         posBo = 350;
         if(!hasWinner){
           hasWinner = true;
-          elementBoWin.style.display = 'unset';
+          field.style.display = 'none';
+           bo_wins.style.display = 'block';
         }
       }
-      bo.style.left = posBo + 'px';
+
     }
   }
 }
